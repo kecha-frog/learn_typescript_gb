11109c62640cc8ce676cd8c17b150d8d0762a014
ke4a <helpomos@gmail.com>
2021-12-02 16:40:31 +0500

    day 4 fix

--- src/test_rent_sdk.ts
+++ src/test_rent_sdk.ts
@@ -1,17 +1,20 @@
 import {FlatRentSdk} from './flat-rent-sdk.js';
 
 export async function test_rent_sdk() {
-  const date = new Date(1638385200000)
-  const date2= new Date(1639508400000)
-
   const flatRent = new FlatRentSdk()
-  const params = {
-    city:'Санкт-Петербург',
-    checkInDate:date,
-    checkOutDate:date2,
-  }
 
   console.log(await flatRent.get('vnd331'))
+
+  const params = {
+    city:'Санкт-Петербург',
+    checkInDate:new Date(1638385200000),
+    checkOutDate:new Date(1639508400000),
+  }
   console.log(await flatRent.search(params))
-  console.log(await flatRent.book('vnd331',date, date2))
+
+  const date1 = new Date()
+  date1.setDate(20)
+  const date2= new Date()
+  date2.setDate(25)
+  console.log(await flatRent.book('vnd331',date1, date2))
 }
